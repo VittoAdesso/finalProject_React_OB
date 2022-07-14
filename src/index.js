b594fb725270517d2d251190201e9777c4efcd23
Vittorio Adesso <adessovittorio8@gmail.com>
2022-07-15 00:05:05 +0200

    import bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// ! bootstrap always above the css styles 
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
